96df8ead99a5e73ef528bb7b350e01a42f2e091d
Richard Borcsik <borcsikrichard@gmail.com>
2019-02-25 19:16:47 +0100

    Add files via upload

---
+++ src/pages/tanulmanyi_18_19_2.jsx
@@ -0,0 +1,36 @@
+import { Helmet } from 'react-helmet';
+import { SocialIcon } from 'react-social-icons';
+import React from 'react';
+import styled, { css } from 'react-emotion';
+
+import NavBar from '../components/navbar';
+import ScolarshipTable from '../components/scolarshiptable';
+
+import osztondijdata from '../osztondij2.json';
+
+const LogoImg = styled.img`
+  height: 50px;
+  margin-bottom: 2rem;
+`;
+
+const MySocialIcon = styled(SocialIcon)`
+  margin: 0 0.5rem;
+`;
+
+export default props => (
+  <div>
+    <Helmet>
+      <title>Tanulmányi ösztöndíjak - 2018/19 Tavaszi félév | RKK HÖK</title>
+      <meta name="description" content="A Rejtő karon a 2018/19-es tanév Tavaszi félévében megitélt tanulmányi ösztöndíjak" />
+    </Helmet>
+
+    <NavBar title="Tanulmányi ösztöndíjak - 2018/19 Tavaszi félév" />
+    <div
+      className={css`
+        margin-top: 30px;
+      `}
+    >
+      <ScolarshipTable keys={osztondijdata.keys} values={osztondijdata.values} />
+    </div>
+  </div>
+);
